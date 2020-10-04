d4e3205679cd10cd800f8576cec0011d993397e6
lmiceli <lmiceli@orderbot.com>
2020-10-03 19:12:25 -0700

    Finishing the project skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,11 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                })
+            );
             services.AddControllers();
         }
 
@@ -47,6 +52,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
